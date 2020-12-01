707ed4d404e10899b147596057da727b4c019db9
palomba93 <gianmarco.palomba@live.it>
2020-12-01 10:47:26 +0100

    main di matlab

--- code_matlab/main.m
+++ code_matlab/main.m
@@ -30,7 +30,11 @@ lesions_mask_foll_img = lesions_mask_foll_halfway.img;
 baseline_date = '13-Apr-2015';
 followup_date = '04-Mar-2019';
 
-[SEL_4, CC_SEL_4] = sel_candidates(log_detJ_img,lesions_mask_base_img,lesions_mask_foll_img,baseline_date,followup_date);
+[SEL_4, CC_SEL_4, J_mask_4] = sel_candidates(log_detJ_img,lesions_mask_base_img,lesions_mask_foll_img,baseline_date,followup_date);
+
+% nifti_J_mask = lesions_mask_base_halfway;
+% nifti_J_mask.img = J_mask_4;
+% save_untouch_nii(nifti_J_mask,'F:\Utente\TESI\Candidati_SEL\000004\J_mask.nii');
 
 %% pz n. 000027
 % baseline 20140630
@@ -48,7 +52,11 @@ lesions_mask_foll_img = lesions_mask_foll_halfway.img;
 baseline_date = '30-Jun-2014';
 followup_date = '15-Jul-2019';
 
-[SEL_27, CC_SEL_27] = sel_candidates(log_detJ_img,lesions_mask_base_img,lesions_mask_foll_img,baseline_date,followup_date);
+[SEL_27, CC_SEL_27, J_mask_27] = sel_candidates(log_detJ_img,lesions_mask_base_img,lesions_mask_foll_img,baseline_date,followup_date);
+
+% nifti_J_mask = lesions_mask_base_halfway;
+% nifti_J_mask.img = J_mask_27;
+% save_untouch_nii(nifti_J_mask,'F:\Utente\TESI\Candidati_SEL\000027\J_mask.nii');
 
 %% pz n. 000128 
 % baseline 20170623
@@ -66,7 +74,11 @@ lesions_mask_foll_img = lesions_mask_foll_halfway.img;
 baseline_date = '23-Jun-2017';
 followup_date = '20-Gen-2020';
 
-[SEL_128, CC_SEL_128] = sel_candidates(log_detJ_img,lesions_mask_base_img,lesions_mask_foll_img,baseline_date,followup_date);
+[SEL_128, CC_SEL_128, J_mask_128] = sel_candidates(log_detJ_img,lesions_mask_base_img,lesions_mask_foll_img,baseline_date,followup_date);
+
+% nifti_J_mask = lesions_mask_base_halfway;
+% nifti_J_mask.img = J_mask_128;
+% save_untouch_nii(nifti_J_mask,'F:\Utente\TESI\Candidati_SEL\000128\J_mask.nii');
 
 %% pz n. 000298
 % baseline 20161121 
@@ -84,6 +96,8 @@ lesions_mask_foll_img = lesions_mask_foll_halfway.img;
 baseline_date = '21-Nov-2016';
 followup_date = '30-Sep-2019';
 
-[SEL_298, CC_SEL_298] = sel_candidates(log_detJ_img,lesions_mask_base_img,lesions_mask_foll_img,baseline_date,followup_date);
+[SEL_298, CC_SEL_298, J_mask_298] = sel_candidates(log_detJ_img,lesions_mask_base_img,lesions_mask_foll_img,baseline_date,followup_date);
 
-
+% nifti_J_mask = lesions_mask_base_halfway;
+% nifti_J_mask.img = J_mask_298;
+% save_untouch_nii(nifti_J_mask,'F:\Utente\TESI\Candidati_SEL\000298\J_mask.nii');
