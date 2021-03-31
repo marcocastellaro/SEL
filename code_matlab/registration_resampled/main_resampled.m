d3cd88e70ba54c3d45c45c55e78b640cc5a0f26f
palomba93 <gianmarco.palomba@live.it>
2021-03-31 19:50:01 +0200

    updates for SEL volume

--- code_matlab/registration_resampled/main_resampled.m
+++ code_matlab/registration_resampled/main_resampled.m
@@ -28,10 +28,16 @@ baseline_date = '11-Apr-2015';
 followup_date = '29-Apr-2019';
 
 [SEL_1142, CC_1142] = sel_candidates(nak_detJ,lesions_prob_base_img,lesions_prob_foll_img,baseline_date,followup_date);
+num_CC = CC_1142.NumObjects;
 
-nifti_1142 = lesions_prob_base_halfway;
-nifti_1142.img = SEL_1142;
-save_untouch_nii(nifti_1142,'F:\Utente\TESI\Output\001142\20150411_20190429\registration_resampled\Jacobian\SEL_1142.nii');
+voxel_SEL = [];
+for i=1:num_CC
+    ind = size(CC_1142.PixelIdxList{1,i});
+    voxel_SEL(i) = ind(1);
+    
+end
+
+volume = sum(voxel_SEL);
 
 %copyfile('F:\Utente\TESI\Output\001142\20150411_20190429\registration_robust_template\FLAIR_T1\Jacobian\SEL_1142_FLAIR.nii','F:\Utente\TESI\Output\001142\20150411_20190429\registration_resampled\Jacobian\SEL_1142_FLAIR.nii','f');
 %copyfile('F:\Utente\TESI\Output\001142\20150411_20190429\registration_robust_template\FLAIR_T1\Jacobian\nak_detJ_FLAIR.nii.gz','F:\Utente\TESI\Output\001142\20150411_20190429\registration_resampled\Jacobian\nak_detJ_FLAIR.nii.gz','f');
