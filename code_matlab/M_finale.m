9e1226c43a37e36a8dc4caaa213db90f953a73a1
palomba93 <gianmarco.palomba@live.it>
2021-04-02 18:53:43 +0200

    resampled T2

--- code_matlab/M_finale.m
+++ code_matlab/M_finale.m
@@ -15,19 +15,23 @@ sienapd_FLAIR = zeros(1,dim);
 sienapd_T2 = zeros(1,dim);
 robust_FLAIR = zeros(1,dim);
 robust_T2 = zeros(1,dim);
+resampled_FLAIR = zeros(1,dim);
+resampled_T2 = zeros(1,dim);
 
-resampled = zeros(1,dim);
 volume_sienapd_FLAIR = zeros(1,dim);
 volume_sienapd_T2 = zeros(1,dim);
 volume_robust_FLAIR = zeros(1,dim);
 volume_robust_T2 = zeros(1,dim);
-volume_resampled = zeros(1,dim);
+volume_resampled_FLAIR = zeros(1,dim);
+volume_resampled_T2 = zeros(1,dim);
 
 k = 1;
 
 result_sienapd = zeros(4,dim);
 result_robust = zeros(4,dim);
-result_resampled = zeros(4,dim);
+result_resampled_FLAIR = zeros(4,dim);
+result_resampled_T2 = zeros(4,dim);
+
 
 for t=3:1:length(list_output)
 
@@ -35,19 +39,25 @@ subjID_output = list_output(t).name;
 subj_path_output= fullfile(output_path, subjID_output);
 subj_output_list=dir(subj_path_output);
 
+try
+CC_SEL_sienapd_FLAIR = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_sienapd','FLAIR_T1','Jacobian','num_CC_SEL_FLAIR.mat'));
+volume_SEL_sienapd_FLAIR = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_sienapd','FLAIR_T1','Jacobian','volume.mat'));
+catch
+    CC_SEL_sienapd_FLAIR.num_CC_SEL_FLAIR = NaN;
+    volume_SEL_sienapd_FLAIR.volume = NaN;
+end
+sienapd_FLAIR(1,k) = CC_SEL_sienapd_FLAIR.num_CC_SEL_FLAIR;
+volume_sienapd_FLAIR(1,k) = volume_SEL_sienapd_FLAIR.volume;
 
-%CC_SEL_sienapd_FLAIR = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_sienapd','FLAIR_T1','Jacobian','num_CC_SEL_FLAIR.mat'));
-%volume_SEL_sienapd_FLAIR = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_sienapd','FLAIR_T1','Jacobian','volume.mat'));
-
-%sienapd_FLAIR(1,k) = CC_SEL_sienapd_FLAIR.num_CC_SEL_FLAIR;
-%volume_sienapd_FLAIR(1,k) = volume_SEL_sienapd_FLAIR.volume;
-
-
-%CC_SEL_sienapd_T2 = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_sienapd','T2_T1','Jacobian','num_CC_SEL_T2.mat'));
-%volume_SEL_sienapd_T2 = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_sienapd','T2_T1','Jacobian','volume.mat'));
-
-%sienapd_T2(1,k) = CC_SEL_sienapd_T2.num_CC_SEL_T2;
-%volume_sienapd_T2(1,k) = volume_SEL_sienapd_T2.volume;
+try
+CC_SEL_sienapd_T2 = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_sienapd','T2_T1','Jacobian','num_CC_SEL_T2.mat'));
+volume_SEL_sienapd_T2 = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_sienapd','T2_T1','Jacobian','volume.mat'));
+catch
+    CC_SEL_sienapd_T2.num_CC_SEL_T2 = NaN;
+    volume_SEL_sienapd_T2.volume = NaN;
+end
+sienapd_T2(1,k) = CC_SEL_sienapd_T2.num_CC_SEL_T2;
+volume_sienapd_T2(1,k) = volume_SEL_sienapd_T2.volume;
 
 try
     
@@ -74,21 +84,33 @@ robust_T2(1,k) = CC_SEL_robust_T2.num_CC_SEL_T2;
 volume_robust_T2(1,k) = volume_SEL_robust_T2.volume;
 
 try
-CC_SEL = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_resampled','Jacobian','num_CC_SEL.mat'));
-volume_SEL = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_resampled','Jacobian','volume.mat'));
+CC_SEL_resampled_FLAIR = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_resampled','FLAIR_T1','Jacobian','num_CC_SEL_FLAIR.mat'));
+volume_SEL_resampled_FLAIR = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_resampled','FLAIR_T1','Jacobian','volume.mat'));
 catch
-    CC_SEL.num_CC_SEL = NaN;
-    volume_SEL.volume = NaN;    
+    CC_SEL_resampled_FLAIR.num_CC_SEL_FLAIR = NaN;
+    volume_SEL_resampled_FLAIR.volume = NaN;    
 end
-resampled(1,k) = CC_SEL.num_CC_SEL;
-volume_resampled(1,k) = volume_SEL.volume;
+resampled_FLAIR(1,k) = CC_SEL_resampled_FLAIR.num_CC_SEL_FLAIR;
+volume_resampled_FLAIR(1,k) = volume_SEL_resampled_FLAIR.volume;
+
+try
+CC_SEL_resampled_T2 = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_resampled','T2_T1','Jacobian','num_CC_SEL_T2.mat'));
+volume_SEL_resampled_T2 = load(fullfile(subj_path_output,subj_output_list(3).name,'registration_resampled','T2_T1','Jacobian','volume.mat'));
+catch
+    CC_SEL_resampled_T2.num_CC_SEL_T2 = NaN;
+    volume_SEL_resampled_T2.volume = NaN;    
+end
+resampled_T2(1,k) = CC_SEL_resampled_T2.num_CC_SEL_T2;
+volume_resampled_T2(1,k) = volume_SEL_resampled_T2.volume;
 
 k = k+1;
 end
 
 result_sienapd = [sienapd_FLAIR ; volume_sienapd_FLAIR ; sienapd_T2 ; volume_sienapd_T2];
 result_robust = [robust_FLAIR ; volume_robust_FLAIR ; robust_T2 ; volume_robust_T2];
-result_resampled = [robust_FLAIR ; volume_robust_FLAIR ; resampled ; volume_resampled];
+result_resampled_FLAIR = [robust_FLAIR ; volume_robust_FLAIR ; resampled_FLAIR ; volume_resampled_FLAIR];
+result_resampled_T2 = [robust_T2 ; volume_robust_T2 ; resampled_T2 ; volume_resampled_T2];
+
 
 
 %save(fullfile(output_summury,'result_sienapd'), 'result_sienapd');
--- code_shell/registration_resampled/main_resampled.sh
+++ code_shell/registration_resampled/main_resampled.sh
@@ -17,5 +17,12 @@ output=/mnt/f/utente/tesi/output/000336/20151214_20190506/
 lesion_mask_baseline=/mnt/f/utente/tesi/dati/000336_20151214/baseline_2ch/baseline_2ch_prob_1.nii.gz
 lesion_mask_follow=/mnt/f/utente/tesi/dati/000336_20190506/baseline_2ch/baseline_2ch_prob_1.nii.gz
 
-registration_resampled $T1_baseline $T1_follow $FLAIR_baseline $FLAIR_follow $T1_brain_baseline $T1_brain_follow $FLAIR_brain_baseline $FLAIR_brain_follow $output -m1 $lesion_mask_baseline -m2 $lesion_mask_follow
+T2_baseline=/mnt/f/utente/tesi/dati/000336_20151214/T2_to_T1.nii.gz
+T1_follow=/mnt/f/utente/tesi/dati/000336_20190506/T2_to_T1.nii.gz
+
+
+T2_brain_baseline=/mnt/f/utente/tesi/dati/000336_20151214/T2_to_T1_brain.nii.gz
+T2_brain_follow=/mnt/f/utente/tesi/dati/000336_20190506/T2_to_T1_brain.nii.gz
+
+registration_resampled $T1_baseline $T1_follow $FLAIR_baseline $FLAIR_follow $T1_brain_baseline $T1_brain_follow $FLAIR_brain_baseline $FLAIR_brain_follow $output -m1 $lesion_mask_baseline -m2 $lesion_mask_follow -t2_b $T2_baseline -t2_f $T2_follow -t2_brain_b $T2_brain_baseline -t2_brain_f $T2_brain_follow
 
